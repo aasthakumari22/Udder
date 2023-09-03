1ab2b5d7e5013ca10e54dfa635cdc23cfae534ca
Aastha kumari <aasthakumari2206@gmail.com>
2023-09-03 14:35:42 +0530

    add viewWillLayoutSubviews.

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
     }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
 
 }
 
